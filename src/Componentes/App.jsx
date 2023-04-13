26ad5bd7b5280c45b45be9b1a7ae479a05312dac
Zamirt Barrera <hzbarrerar@unac.edu.co>
2023-04-13 14:40:45 -0500

    Modificaciones a los archivos y se eliminaron los archivos de log reportweb, setup, app.css, js, test.js

---
+++ src/Componentes/App.jsx
@@ -0,0 +1,15 @@
+export function App() {
+  return (
+    <div>
+      <header>
+        <h1> Sistema de Alenta Temprana </h1>
+      </header>
+      <main>
+        <li>Informes</li>
+        <li>Caracteristicas</li>
+        <li>Perfil</li>
+        <li>Herramientas</li>
+      </main>
+    </div>
+  );
+}
---
+++ src/Componentes/Login.jsx
@@ -0,0 +1,9 @@
+export function Login() {
+  return (
+    <div>
+      <header>
+        <h1>Login</h1>
+      </header>
+    </div>
+  );
+}
