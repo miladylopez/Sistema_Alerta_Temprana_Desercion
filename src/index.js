da6f445c69c2e784d1628f7ecbed1b5733fbe3fb
Zamirt Barrera <hzbarrerar@unac.edu.co>
2023-06-05 23:34:21 -0500

    Modificaciones en index.js y principio de algunos ajustes para el backend

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import { Login} from "./Componentes/Login";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    
     <Login />
-    
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
