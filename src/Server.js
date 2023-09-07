4f6b4914b4441eaf6c5ca150c4b7fdd2d284aa83
“Zamirt <hzbarrerar@unac.edu.co>
2023-09-06 23:07:21 -0500

    Conectar las bases de datos

--- src/Server.js
+++ src/Server.js
@@ -9,19 +9,28 @@ const port = process.env.PORT || 3000;
 
 // Configura la conexión a tu base de datos MariaDB.
 const pool = mariadb.createPool({
-  host: 'tu_host_de_MariaDB',
-  user: 'tu_usuario_de_MariaDB',
-  password: 'tu_contraseña_de_MariaDB',
-  database: 'tu_base_de_datos',
+  host: 'localhost',
+  user: 'user_sistema',
+  password: '123456789',
+  database: 'Aspirante',
   connectionLimit: 5, // Puedes ajustar este límite según tus necesidades.
 });
 
 // Middleware para permitir el análisis de solicitudes JSON.
 app.use(express.json());
 
-// Rutas de ejemplo
-app.get('/', (req, res) => {
-  res.send('¡Hola desde Express!');
+// Ruta para obtener datos de la base de datos.
+app.get('/api/obtener-datos', async (req, res) => {
+  try {
+    const conn = await pool.getConnection();
+    const rows = await conn.query('SELECT * FROM tu_tabla');
+    conn.release();
+
+    res.json(rows); // Enviar los datos como respuesta JSON.
+  } catch (err) {
+    console.error('Error al obtener datos:', err);
+    res.status(500).json({ error: 'Error al obtener datos' });
+  }
 });
 
 // Inicia el servidor en el puerto especificado.
