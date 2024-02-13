1214966c724a6a69d5c228f2d1b6b75ff4d50a28
Zamirt <zamirt_tymo_zt@hotmail.com>
2024-02-13 09:44:02 -0500

    Importacion de css en lo popup y se crea el css depopup

--- src/Componentes/Popup/Habilidades/Popup_Activiades_interes.jsx
+++ src/Componentes/Popup/Habilidades/Popup_Activiades_interes.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import '../../Popup.css';
 
 function Subcriterio4() {
 
--- src/Componentes/Popup/Habilidades/Popup_Contexto_educativo.jsx
+++ src/Componentes/Popup/Habilidades/Popup_Contexto_educativo.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-
+import '../../Popup/Popup.css';
 function Subcriterio5() {
 
   const [criteriaScores, setCriteriaScores] = useState({});
