df1d038da12149ea1f83d51ac57fcbca64deb538
Zamirt <zamirt_tymo_zt@hotmail.com>
2024-03-05 11:15:40 -0500

    Ajustes en los modal de presentacion y estilos

--- src/Componentes/Criterios/SubCriterios/Presentacion.jsx
+++ src/Componentes/Criterios/SubCriterios/Presentacion.jsx
@@ -6,8 +6,8 @@ import '../SubCriterios/SubCriterios.css'; // Importar los estilos
 const Presentacion = () => {
   const [criteriaScores, setCriteriaScores] = useState({});
   const [criterios, setCriterios] = useState([
-    { titulo: "Aspecto Personal", id: "Crit5" },
-    { titulo: "Comunicacion Oral", id: "Crit6" }
+    { titulo: "Aspecto Personal", id: "Crit5", contenido: ["- Impacto general que genera el estudiante (agradable, confianza, seguridad, etc).", "- Forma de vestir (adecuada, llamatica, etc)."] },
+    { titulo: "Comunicacion Oral", id: "Crit6", contenido: ["- La comunicación oral se evalúa en la medida que se va desarrollando la entrevista con las preguntas que se realicen al estudiante.", "- Claridad, seguridad, lógica, riqueza de vocabulario.", "- Contacto visual.", "- Postura.", "- Gesticulación.", "- Tono, timbre, volumen."] }
   ]);
 
   // Estados para controlar la visibilidad de los modales
@@ -20,6 +20,11 @@ const Presentacion = () => {
   // Estado para controlar el texto del nuevo criterio
   const [newCriterioText, setNewCriterioText] = useState('');
 
+  // Estado para controlar si el contenido del modal está en modo de edición o no
+  const [editMode, setEditMode] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+  const [editedContent, setEditedContent] = useState('');
+
   const handleScoreChange = (criterioId, score) => {
     setCriteriaScores(prevScores => ({ ...prevScores, [criterioId]: score }));
   };
@@ -30,7 +35,7 @@ const Presentacion = () => {
   };
 
   const handleAddCriterio = (text) => {
-    const newCriterio = { titulo: text, id: uuidv4() };
+    const newCriterio = { titulo: text, id: uuidv4(), contenido: [] };
     setCriterios(prevCriterios => [...prevCriterios, newCriterio]);
     setNewCriterioText('');
   };
@@ -40,14 +45,30 @@ const Presentacion = () => {
   };
 
   // Funciones para mostrar/ocultar los modales
-  const toggleAspectoPersonalModal = () => {
-    setShowAspectoPersonalModal(prevState => !prevState);
+  const toggleModal = (criterioId) => {
+    if (criterioId === "Crit5") {
+      setShowAspectoPersonalModal(prevState => !prevState);
+    } else if (criterioId === "Crit6") {
+      setShowComunicacionOralModal(prevState => !prevState);
+    }
   };
 
-  const toggleComunicacionOralModal = () => {
-    setShowComunicacionOralModal(prevState => !prevState);
+  // Función para cambiar el modo de edición
+  const toggleEditMode = (index) => {
+    setEditMode(true);
+    setEditIndex(index);
+    setEditedContent(criterios[index].contenido.join('\n'));
   };
-  
+
+  const handleEdit = () => {
+    const updatedCriterios = [...criterios];
+    updatedCriterios[editIndex].contenido = editedContent.split('\n');
+    setCriterios(updatedCriterios);
+    setEditMode(false);
+    setEditIndex(null);
+    setEditedContent('');
+  };
+
   // Funciones para avanzar y retroceder
   const handleForward = () => {
     // Lógica para avanzar
@@ -77,10 +98,10 @@ const Presentacion = () => {
         <br />
 
         {/* Renderizar los criterios y sus respectivos modales */}
-        {criterios.map((criterio) => (
+        {criterios.map((criterio, index) => (
           <div key={criterio.id}>
             <div className="criterio-box">
-              <button className='criterio-button-presentacion' onClick={criterio.id === "Crit5" ? toggleAspectoPersonalModal : toggleComunicacionOralModal}>
+              <button className='criterio-button-presentacion' onClick={() => toggleModal(criterio.id)}>
                 {criterio.titulo}
               </button>
               {/* Input numérico */}
@@ -94,31 +115,74 @@ const Presentacion = () => {
               <button className="delete-button" onClick={() => handleDeleteCriterio(criterio.id)}>
                 <BsTrashFill />
               </button>
+              {/* Botón para editar */}
+              
             </div>
           </div>
         ))}
 
-        {/* Modal para el subcriterio "Aspecto Personal" */}
-        {showAspectoPersonalModal && (
-          <div className="modal">
-            <div className="modal-content">
-              <h2>Información sobre Aspecto Personal</h2>
-              {/* Contenido específico de Aspecto Personal */}
-              <button onClick={toggleAspectoPersonalModal}>Cerrar</button>
-            </div>
-          </div>
-        )}
+        {/* Modales para los criterios */}
+        {criterios.map((criterio, index) => (
+          <div key={criterio.id}>
+            {showAspectoPersonalModal && criterio.id === "Crit5" && (
+              <div className="modal-sub">
+                <div className="modal-content-sub">
+                  <i className="fa-solid fa-list"></i>
+                  <h1>Para tener en cuenta</h1>
+                  <h2>{criterio.titulo}</h2>
+                  {editMode && editIndex === index ? (
+                    // Modo de edición
+                    <textarea
+                      value={editedContent}
+                      onChange={(e) => setEditedContent(e.target.value)}
+                    ></textarea>
+                  ) : (
+                    // Modo de visualización
+                    criterio.contenido.map((item, i) => (
+                      <p key={i}>{item}</p>
+                    ))
+                  )}
+                  {/* Botones para cerrar y editar/guardar */}
+                  <button onClick={() => toggleModal(criterio.id)}>Cerrar</button>
+                  {editMode && editIndex === index ? (
+                    <button onClick={handleEdit}>Guardar</button>
+                  ) : (
+                    <button onClick={() => toggleEditMode(index)}>Editar</button>
+                  )}
+                </div>
+              </div>
+            )}
 
-        {/* Modal para el subcriterio "Comunicacion Oral" */}
-        {showComunicacionOralModal && (
-          <div className="modal">
-            <div className="modal-content">
-              <h2>Información sobre Comunicación Oral</h2>
-              {/* Contenido específico de Comunicacion Oral */}
-              <button onClick={toggleComunicacionOralModal}>Cerrar</button>
-            </div>
+            {showComunicacionOralModal && criterio.id === "Crit6" && (
+              <div className="modal-sub">
+                <div className="modal-content-sub">
+                  <i className="fa-solid fa-list"></i>
+                  <h1>Para tener en cuenta</h1>
+                  <h2>{criterio.titulo}</h2>
+                  {editMode && editIndex === index ? (
+                    // Modo de edición
+                    <textarea
+                      value={editedContent}
+                      onChange={(e) => setEditedContent(e.target.value)}
+                    ></textarea>
+                  ) : (
+                    // Modo de visualización
+                    criterio.contenido.map((item, i) => (
+                      <p key={i}>{item}</p>
+                    ))
+                  )}
+                  {/* Botones para cerrar y editar/guardar */}
+                  <button onClick={() => toggleModal(criterio.id)}>Cerrar</button>
+                  {editMode && editIndex === index ? (
+                    <button onClick={handleEdit}>Guardar</button>
+                  ) : (
+                    <button onClick={() => toggleEditMode(index)}>Editar</button>
+                  )}
+                </div>
+              </div>
+            )}
           </div>
-        )}
+        ))}
 
         <div className="add-crit">
           {/* Botón para abrir el modal de agregar nuevo criterio */}
