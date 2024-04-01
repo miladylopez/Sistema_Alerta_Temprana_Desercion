5486bd1166171474df975e4249f2fd64333af0a7
Zamirt <zamirt_tymo_zt@hotmail.com>
2024-04-01 00:43:15 -0500

    Se crea la conexión a las bases de datos para hacer primera pruebas

--- ConexionDB.js
+++ ConexionDB.js
@@ -19,10 +19,10 @@ app.post("/create", (req, res) => {
   const codigo_carnet = req.body.codigo_carnet;
   const email_aspirante = req.body.email_aspirante;
   const telefono_aspirante = req.body.telefono_aspirante;
-  const id_periodo = req.body.id_periodo; 
-  const id_entrevistador = req.body.id_entrevistador; 
+  const id_periodo = req.body.id_periodo;
+  const id_entrevistador = req.body.id_entrevistador;
 
-  connection.query('INSERT INTO aspirantes(nombre_aspirante, codigo_carnet, email_aspirante, telefono_aspirante, id_periodo,id_entrevistador ) VALUES (?, ?, ?, ?, ?, ?)', 
+  connection.query('INSERT INTO aspirantes(nombre_aspirante, codigo_carnet, email_aspirante, telefono_aspirante, id_periodo,id_entrevistador ) VALUES (?, ?, ?, ?, ?, ?)',
   [nombre_aspirante, codigo_carnet, email_aspirante, telefono_aspirante, id_periodo,id_entrevistador], (err, result) => {
     if (err) {
       console.log(err);
@@ -63,7 +63,7 @@ app.delete("/delete/:id_aspirante", (req, res) => {
   const id_aspirante = req.params.id_aspirante;
   
   connection.query('DELETE FROM aspirantes WHERE id_aspirante= ?',id_aspirante,
-   (err, result) => {
+    (err, result) => {
     if (err) {
       console.log(err);
     } else {
@@ -97,3 +97,9 @@ app.get("/entrevistador", (req, res) => {
 app.listen(3001,()=>{
   console.log("Corriendo en el puerto 3001")
 })
+
+
+
+
+//port 3310
+
